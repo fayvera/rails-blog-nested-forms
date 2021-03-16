d066e1e92a075ffbb65fb4328f88dc3c4ef1bcb7
Fay Vera <fvera.cavalcante@gmail.com>
2021-03-15 20:56:23 -0400

    commit tree bf36f3fb8e7c5f33dee213b9c57e6385db13b0e2

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,5 +2,7 @@ class Tag < ActiveRecord::Base
   has_many :post_tags
   has_many :posts, :through => :post_tags
 
-  validates_uniqueness_of :name
+  # accepts_nested_attributes_for :posts, reject_if: proc { |attributes| attributes['title'].blank? }
+  # validates_uniqueness_of :name
+  validates :name, uniqueness: true, presence: true
 end
